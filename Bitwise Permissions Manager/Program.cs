909aadda65a174054591e71efd196d8ab489b983
hamzalafsioui <hamzalafsioui@gmail.com>
2024-07-12 21:18:09 +0000

    writing First Example Code for Bitwise Operations

---
+++ Bitwise Permissions Manager/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Bitwise_Permissions_Manager
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			const byte FLAG_READ = 0x01;
+			const byte FLAG_WRITE = 0x02;
+			const byte FLAG_EXECUTE = 0x04;
+			const byte FLAG_DELETE = 0x08;
+
+			Byte Permissions = FLAG_READ | FLAG_WRITE;
+			bool CanRead = (Permissions & FLAG_READ) != 0;
+			bool CanWite = (Permissions & FLAG_WRITE) != 0;
+			bool CanExecute = (Permissions & FLAG_EXECUTE) != 0;
+			bool CanDelete = (Permissions & FLAG_DELETE) != 0;
+
+			Console.WriteLine($"User can read: {CanRead}");      // True
+			Console.WriteLine($"User can write: {CanWite}");    // True
+			Console.WriteLine($"User can execute: {CanExecute}"); // False
+			Console.WriteLine($"User can delete: {CanDelete}");    // False
+			Console.WriteLine(Convert.ToString(Permissions, 2).PadLeft(8, '0'));
+
+			Console.WriteLine("After Adding FLAG_DELETE ");
+			Permissions |= FLAG_DELETE; // using | to add FLAG_DELETE
+			Console.WriteLine(Convert.ToString(Permissions, 2).PadLeft(8, '0'));
+
+			Console.WriteLine("After Removing FLAG_WRITE");
+			Permissions = Convert.ToByte(Permissions & ~FLAG_WRITE); // using ~ to inverse byte (Ex: 1001 to 0110)
+			Console.WriteLine(Convert.ToString(Permissions, 2).PadLeft(8, '0'));
+
+			CanRead = (Permissions & FLAG_READ) != 0;
+			CanWite = (Permissions & FLAG_WRITE) != 0;
+			CanExecute = (Permissions & FLAG_EXECUTE) != 0;
+			CanDelete = (Permissions & FLAG_DELETE) != 0;
+
+			Console.WriteLine($"User can read: {CanRead}");        // True
+			Console.WriteLine($"User can write: {CanWite}");       // False
+			Console.WriteLine($"User can execute: {CanExecute}");  // False
+			Console.WriteLine($"User can delete: {CanDelete}");    // True
+
+            Console.WriteLine("\n\n Real Example class User\n");
+
+
+
+
+            Console.ReadKey();
+		}
+	}
+}
---
+++ Bitwise Permissions Manager/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bitwise_Permissions_Manager
+{
+	public class User
+	{
+		public enum enPermissions
+		{
+			Add = 1,
+			Update = 2,
+			Delete = 4,
+			ViewReports = 8,
+			ManageUsers = 16,
+			ExportData = 32,
+			ApproveRequests = 64,
+			AdministerSystem = 128
+
+
+		}
+		public int Id { get; set; }
+		public string Name { get; set; }
+
+		public int Permissions {  get; set; }
+
+	}
+
+	
+}
